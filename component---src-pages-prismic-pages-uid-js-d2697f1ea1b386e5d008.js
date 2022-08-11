25baaf52cafe4d83cbacb5f68a2959387533b10b
Shakeel Shafiq <shakeel.shafique@gmail.com>
2022-08-10 21:16:18 -0400

    Updates

---
+++ component---src-pages-prismic-pages-uid-js-d2697f1ea1b386e5d008.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[723],{8793:function(t,e,a){a.r(e);var l=a(7294),r=a(5127),s=a(3751);e.default=function(t){return console.log(t),l.createElement(r.Z,null,l.createElement(s.Z,{title:t.data.prismicPages.data.title.text}),l.createElement("h1",null,t.data.prismicPages.data.title.text))}}}]);
+//# sourceMappingURL=component---src-pages-prismic-pages-uid-js-d2697f1ea1b386e5d008.js.map
